3d7927db6abc0318337eb4a3dc53b7a299407be0
Jas K. Benipal <benipal.jas24@gmail.com>
2015-01-17 14:41:45 -0600

    Adding demangler utility.

--- algo_base.h
+++ algo_base.h
@@ -3,19 +3,29 @@
 namespace dstl {
 
 template<typename FwdIt>
-void rotate(FwdIt first, FwdIt n_first, FwdIt last)
+void rotate(FwdIt first, FwdIt n_first, FwdIt end)
 {
     FwdIt middle = n_first;
     while (first != middle)
     {
         dstl::iter_swap(first++, middle++);
-        if (middle == last) {
-            dstl::rotate(first, n_first, last);
+        if (middle == end) {
+            dstl::rotate(first, n_first, end);
             break;
         } else if (first == n_first) {
-            dstl::rotate(first, middle, last);
+            dstl::rotate(first, middle, end);
             break;
         }
     }
 }
+#if 0
+template<typename RandomIterator, typename T>
+RandomIterator
+binary_search(RandomIterator start, RandomIterator end, const T& value) 
+{
+   if (start == end) return end;
+   auto = std::distance(start, end);
+
+}
+#endif
 } // namespace dstl
--- utilities.h
+++ utilities.h
@@ -1,4 +1,8 @@
 #include <utility>
+#include <typeinfo>
+#include <cxxabi.h>
+#include <string>
+#include <cstdlib>
 
 namespace dstl {
 
@@ -6,5 +10,18 @@ template<typename Iterator>
 void iter_swap(Iterator it1, Iterator it2) {
     using std::swap;
     swap(*it1, *it2);
+}
+
+template<typename T>
+std::string demangler(const T& x)
+{
+    int status;
+    char *realname;
+
+    realname = abi::__cxa_demangle(typeid(x).name(),0,0,&status);
+    std::string output(realname);
+    free(realname);
+
+    return output;
 }
 } // namespace dstl
